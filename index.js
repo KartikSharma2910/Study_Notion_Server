0ec938e31ae76ca389c8bb49df666e0e1cbf00e5
Kartik Sharma <kartiksharma200029@gmail.com>
2024-01-21 23:52:49 +0530

    Db Setup

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+/* All Imports */
 const express = require("express");
+require("dotenv").config();
 
+/* Creating App Instanse */
 const app = express();
 
+/* Including Middleware */
 app.use(express.json());
+
+/* Basic App Requirements for Creating Server */
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server Successfully started at ${PORT}`));
